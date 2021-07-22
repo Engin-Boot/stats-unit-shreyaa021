ba4260cffaaa601fa78d49ab2656de91d5d39106
shrp <shreya.p@publicissapient.com>
2021-07-22 17:23:47 +0530

    no message

--- Statistics/StatisticsDataModel.cs
+++ Statistics/StatisticsDataModel.cs
@@ -5,28 +5,30 @@ using System.Text;
 
 namespace Statistics
 {
-   public class StatisticsDataModel
+    public class StatisticsDataModel
     {
         public double max;
-       public double min;
-       public double average  ;
+        public double min;
+        public double average;
+
+
+
+
+        public StatisticsDataModel(double max, double min, double average)
+        {
+            this.max = max;
+            this.min = min;
+            this.average = average;
 
 
         }
-}
-    public StatisticsDataModel(double max ,double min ,double average)
-{
-    this.max = max;
-    this.min = min;
-    this.average = average;
+        public StatisticsDataModel()
+        {
+            this.max = Double.NAN;
+            this.min = Double.NAN;
+            this.average = Double.NAN;
 
 
-}
-public StatisticsDataModel()
-{
-    this.max = Double.NAN;
-    this.min = Double.NAN;
-    this.average = Double.NAN;
-
-
+        }
+    }
 }
