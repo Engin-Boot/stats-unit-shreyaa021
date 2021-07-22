2fa1f1ace467fde1f1ea558c1bbc1dc6da1bca88
shrp <shreya.p@publicissapient.com>
2021-07-22 18:01:05 +0530

    no message

--- Statistics/StatsComputer.cs
+++ Statistics/StatsComputer.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Statistics
+  namespace Statistics
 {
     public class StatsComputer
     {
 
-     
 
-        public  StatisticsDataModel CalculateStatistics(List<double> numbers) {
+
+        public StatisticsDataModel CalculateStatistics(List<double> numbers)
+        {
 
 
         public double average;
@@ -19,26 +20,28 @@ namespace Statistics
             min = numbers[0];
             average = 0;
             if (numbers.Count == 0)
+             return new StatisticsDataModel();
+    
+           
+                for (int i=0; i<numbers.Count;i++) 
             {
-                return new StatisticsDataModel();
-            }
-            else
-            {
-                for (int i=0; i< numbers.Count;i++)
-                {
+                
                     if (numbers[i] > max)
                         max = numbers[i];
                     if (numbers[i] < min)
                         min = numbers[i];
                     average += numbers[i];
-                }
+            }
+                
                 average = average / numbers.Count;
+        
                 return (new StatisticsDataModel(max, min, average));
 
+            
+
+
             }
-
-
-
         }
     }
-}
+
+
