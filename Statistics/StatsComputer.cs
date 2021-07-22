439b4ddc62291cfb7416e86ecf43580c17169bf9
shrp <shreya.p@publicissapient.com>
2021-07-22 21:15:59 +0530

    no message

--- Statistics/StatsComputer.cs
+++ Statistics/StatsComputer.cs
@@ -8,26 +8,27 @@ using System.Collections.Generic;
 
         public StatisticsDataModel CalculateStatistics(List<float> numbers)
         {   
-       double average= numbers[0];
-       double max =numbers[0];
-        double min=numbers[0];
-
+        
            
           
             var sum = 0f;
             if (numbers.Count == 0)
-            {
-                max = Double.NaN;
-                min = Double.NaN;
-                average = Double.NaN;
-                
-	
+            {              
+              
+              return new StatisticsDataModel(Double.NaN,Double.NaN,Double.NaN);
             }
+            
+
             else {
+
+                 double average= numbers[0];
+                 double max =numbers[0];
+                 double min=numbers[0];
            
                 foreach(var number in numbers)
             {
                 
+
                     if (number > max)
                         max = number;
                     if (number < min)
